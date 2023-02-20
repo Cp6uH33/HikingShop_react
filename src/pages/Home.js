cd07045cc01a95466d627264320eb00f9e8b1a70
Gnjale <ignjatije.i@gmail.com>
2023-02-20 21:19:16 +0100

    Kreirane komponente i razne stranicee

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import FeaturedItems from '../components/FeaturedItems';
+import HeroHeader from '../components/HeroHeader';
+import Subscribe from '../components/Subscribe';
+
+const Home = () => {
+  return (
+    <>
+    
+    <HeroHeader/>
+
+    <FeaturedItems/>
+
+    <Subscribe/>
+
+    </>
+  )
+}
+
+export default Home;
